7231072189c2ae924e418e212abbd48985a3b75f
Adam <50333588+adamcpenman@users.noreply.github.com>
2019-08-22 20:32:00 -0500

    lesson2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React, { useState } from "react";
 import Heading from "./Heading";
 import Reset from "./Reset";
 
+const data = [
+  { text: "Hello world" },
+  { text: "Hello Galaxy" },
+  { text: "Hello Universe" }
+];
+
 function App() {
   const [count, updateCount] = useState(40);
 
   return (
     <div>
-      <Heading text="Hello World" />
-      <Heading text="Hello Universe" />
-      <Heading text={count} />
+      {/* <Heading text="Hello World" />
+      <Heading text="Hello Universe" /> */}
+      <Heading text={`Count: ${count}`} />
+
+      {data.map((item, index) => {
+        return <Heading key={index} text={item.text} />;
+      })}
 
       <h4>Current count: {count}</h4>
       <button onClick={() => updateCount(count + 1)}>Increment</button>
