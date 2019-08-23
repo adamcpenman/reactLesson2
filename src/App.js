df68509e3b71b6735597bd31b6bbc12ed06f26cf
Adam <50333588+adamcpenman@users.noreply.github.com>
2019-08-22 20:06:10 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Heading from "./Heading";
+import Reset from "./Reset";
+
+function App() {
+  const [count, updateCount] = useState(40);
+
+  return (
+    <div>
+      <Heading text="Hello World" />
+      <Heading text="Hello Universe" />
+      <Heading text={count} />
+
+      <h4>Current count: {count}</h4>
+      <button onClick={() => updateCount(count + 1)}>Increment</button>
+      <button onClick={() => updateCount(count - 1)}>Decrement</button>
+      <Reset changeTheCountValue={updateCount} />
+
+      {count === 42 && (
+        <p>
+          You have found the answer to the ultimate question of life, the
+          universe, and everything.
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default App;
